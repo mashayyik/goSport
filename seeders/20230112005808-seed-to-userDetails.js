ff83b00c2155dfeeabda86e62d02baa998de326c
mashayyik <mashayyik@gmail.com>
2023-01-12 08:33:49 +0700

    prepare seed HLR

--- seeders/20230112005808-seed-to-userDetails.js
+++ seeders/20230112005808-seed-to-userDetails.js
@@ -1,8 +1,10 @@
 'use strict';
+const fs = require('fs');
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up (queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,14 +14,22 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    const userdetails = JSON.parse(fs.readFileSync('./data/userdetails.json', 'utf-8'))
+        .map(el =>{
+          el.createdAt = el.updatedAt = new Date()
+          return el
+        })
+    return queryInterface.bulkInsert('UserDetails', userdetails, {})
+
   },
 
-  async down (queryInterface, Sequelize) {
+  down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    return queryInterface.bulkDelete('UserDetails', null, {})
   }
 };
