05b43724e8b417d7f8f1340943fe564a880775d2
mmacnesa <mmacnesa@gmail.com>
2023-01-12 14:28:39 +0700

    macnesaSiang

--- models/transaction.js
+++ models/transaction.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Transaction.belongsTo(models.Field)
       // define association here
     }
   }
