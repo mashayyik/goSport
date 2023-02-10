dcee28485daa55a2b694ea56b893a91762e76ccc
mashayyik <mashayyik@gmail.com>
2023-02-10 22:07:53 +0700

    feat: PORT

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const session = require('express-session')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const session = require("express-session");
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-app.use(express.static('public'))
-app.use(session({
-  secret: 'xixixi123',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { sameSite:true,
-    secure: false }
-}))
-app.use(require('./routes'))
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: "xixixi123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { sameSite: true, secure: false },
+  })
+);
+app.use(require("./routes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
